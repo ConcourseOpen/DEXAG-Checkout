da6559d1adf74ffd6c7c94e24e8582f94d112c01
roni <roni.davelman@gmail.com>
2019-04-30 21:02:39 -0400

    integrate sdk into checkout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {DexAgSdk} from 'dexag-sdk'
+
 //import Work from './Components/Work'
 
 class App extends Component {
@@ -8,23 +10,31 @@ class App extends Component {
       data: {}
     }
   }
+  componentDidMount(){
+	  var sdk = new DexAgSdk()
+
+	  sdk.getBest({})
+	  .then((result)=>{
+		  console.log(result)
+	  })
+  }
   render() {
     return (
-      <div className="app">
-		  <div className="container">
-			  <h3 className="title">Checkout with DEX.AG</h3>
-			  <select>
-				  <option value="ETH" selected="selected">Ethereum</option>
-			  </select>
-			  <select>
-				  <option value="ETH" selected="selected">DAI</option>
-			  </select>
-			  <button onClick="alert()">Buy</button>
-			  <div className="status-message">
-				  <h3>Waiting to be mined</h3>
-			  </div>
-		  </div>
-      </div>
+	<div className="app">
+		<div className="container">
+			<h3 className="title">Checkout with DEX.AG</h3>
+			<select>
+				<option value="ETH" selected="selected">Ethereum</option>
+			</select>
+			<select>
+			<option value="ETH" selected="selected">DAI</option>
+				</select>
+			<button onClick="alert()">Buy</button>
+			<div className="status-message">
+				<h3>Waiting to be mined</h3>
+			</div>
+		</div>
+	</div>
     );
   }
 }
